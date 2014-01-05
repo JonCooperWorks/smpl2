dba3646fbdd5456bbcfab48bf1c3c426a9637fb1
Kenardo <headnoegoodagain@hotmail.com>
2014-01-04 22:47:10 -0500

    files removed

--- smpl/src/smpl/lang/ASTNode.java
+++
@@ -1,8 +0,0 @@
-package smpl.lang;
-
-public abstract class ASTNode {
-    public abstract Object visit(Visitor v, Object info) throws SmplException;
-    public abstract String toString();
-    public abstract ASTNode getChild(int i);
-    public abstract int getChildCount();
-}
